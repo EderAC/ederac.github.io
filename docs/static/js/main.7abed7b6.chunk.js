610d9d899b1f75f8eb5587af04d2cfaca858a700
EderAC <eder.almeida40@gmail.com>
2020-09-23 11:46:54 -0300

    builded my app

---
+++ docs/static/js/main.7abed7b6.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpederac.github.io"]=this["webpackJsonpederac.github.io"]||[]).push([[0],{3:function(e,t,n){e.exports=n(8)},8:function(e,t,n){"use strict";n.r(t);var r=n(0),a=n.n(r),c=n(2);var o=function(){return a.a.createElement("h1",null,"My site works")};n.n(c).a.render(a.a.createElement(a.a.StrictMode,null,a.a.createElement(o,null)),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.7abed7b6.chunk.js.map
